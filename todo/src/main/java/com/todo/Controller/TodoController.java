b0c8f45a755aa27efa3a65f58c8f4eb463241bad
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-23 08:28:01 +0530

    todo controller

--- todo/src/main/java/com/todo/Controller/TodoController.java
+++ todo/src/main/java/com/todo/Controller/TodoController.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.todo.Entities.Todo;
+import com.todo.Dto.TodoDto;
+import com.todo.ResponseDto.TodoResponseDto;
 import com.todo.Services.TodoService;
 
 import java.util.List;
@@ -20,27 +21,27 @@ public class TodoController {
     private TodoService todoService;
 
     @PostMapping("/todos")
-    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
-        Todo newTodo = todoService.addTodo(todo);
+    public ResponseEntity<TodoResponseDto> addTodo(@RequestBody TodoDto todo) {
+        TodoResponseDto newTodo = todoService.addTodo(todo);
         return new ResponseEntity<>(newTodo, HttpStatus.CREATED);
     }
 
     @GetMapping("/todos")
-    public ResponseEntity<List<Todo>> getAllTodos() {
-        List<Todo> todos = todoService.getAllTodos();
+    public ResponseEntity<List<TodoResponseDto>> getAllTodos() {
+        List<TodoResponseDto> todos = todoService.getAllTodos();
         return ResponseEntity.ok(todos);
     }
 
 
     @GetMapping("/todos/{id}")
-    public ResponseEntity<Todo> getTodoUsingId(@PathVariable("id") String id) {
-        Todo todo = todoService.getTodoById(id);
+    public ResponseEntity<TodoResponseDto> getTodoUsingId(@PathVariable("id") String id) {
+        TodoResponseDto todo = todoService.getTodoById(id);
         return ResponseEntity.ok(todo);
     }
 
     @PutMapping("/todos/update/{id}")
-    public ResponseEntity<Todo> updateTodo(@PathVariable("id") String id, @RequestBody Todo todoDetails) {
-        Todo updatedTodo = todoService.updateTodo(id, todoDetails);
+    public ResponseEntity<TodoResponseDto> updateTodo(@PathVariable("id") String id, @RequestBody TodoDto todoDetails) {
+        TodoResponseDto updatedTodo = todoService.updateTodo(id, todoDetails);
         return ResponseEntity.ok(updatedTodo);
     }
 
